adaa2179f8c0c5b70f58ab4d6b8d5033c8e687d9
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-10 17:51:02 +0700

    Repository

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/RoleRepository.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/RoleRepository.java
@@ -1,5 +1,14 @@
 package vn.com.unit.repository;
 
-public class RoleRepository {
+import java.util.List;
 
+import org.springframework.data.mirage.repository.MirageRepository;
+import org.springframework.data.repository.query.Param;
+
+import vn.com.unit.entity.Role;
+
+public interface RoleRepository extends MirageRepository<Role, Long> {
+
+	public List<Role> findRoleByAccountId(@Param("account_id") Long account_id);
+	
 }
