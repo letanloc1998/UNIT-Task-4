8a0f17258f3b3ea2ab2e67bdbec0b7b4b5e520e6
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-10 17:33:53 +0700

    Update Account.java

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/entity/Account.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/entity/Account.java
@@ -1,5 +1,63 @@
 package vn.com.unit.entity;
 
+import java.util.List;
+
 public class Account {
 
+	private Long id;
+	
+	private String name;
+
+	private String username;
+
+	private String password;
+	
+	private Boolean active;
+
+	private List<Role> roles;
+	
+	public Account() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	
 }
