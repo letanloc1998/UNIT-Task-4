23035d31a245c7d565dd9b37eb78e00f355f75df
trantrungkhanh <62841543+trantrungkhanh@users.noreply.github.com>
2020-08-10 18:14:23 +0700

    login, register form

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/HomeController.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/HomeController.java
@@ -18,6 +18,12 @@ public class HomeController {
         return new ModelAndView("index");
     }
 	
+	@RequestMapping("/register")
+    public ModelAndView register(Model model) {
+
+        return new ModelAndView("register");
+    }
+	
 	@RequestMapping("/login")
 	public ModelAndView login(Model model) {
 
