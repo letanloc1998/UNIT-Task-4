e155c40bc149a79a4007200f61d0a9db46c832e6
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-10 15:59:59 +0700

    Update Run Server

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/HomeController.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/HomeController.java
@@ -1,5 +1,17 @@
 package vn.com.unit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class HomeController {
 
+	@RequestMapping("*")
+    public ModelAndView home(Model model) {
+
+        return new ModelAndView("index");
+    }
+	
 }
