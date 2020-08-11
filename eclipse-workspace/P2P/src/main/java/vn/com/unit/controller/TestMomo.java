40e13f32c2b0dc7040f5d0f4f9bf7368b67d11f4
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-11 15:48:30 +0700

    Momo

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/TestMomo.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/TestMomo.java
@@ -11,16 +11,83 @@ import com.mservice.shared.constants.Parameter;
 //import com.mservice.shared.constants.ProcessType;
 import com.mservice.shared.constants.RequestType;
 import com.mservice.shared.sharedmodels.Environment;
+import com.mservice.shared.sharedmodels.Environment.EnvTarget;
+import com.mservice.shared.sharedmodels.Environment.ProcessType;
 import com.mservice.shared.sharedmodels.PartnerInfo;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+//import com.mservice.allinone.processor.allinone.PayATM;
+
+//import com.mservice.allinone.PayGate;
+import com.mservice.allinone.models.CaptureMoMoResponse;
+import com.mservice.allinone.models.PayATMResponse;
+import com.mservice.allinone.models.PayGateRequest;
+import com.mservice.allinone.models.QueryStatusTransactionResponse;
+import com.mservice.allinone.models.RefundMoMoResponse;
+import com.mservice.allinone.processor.allinone.*;
+import com.mservice.shared.exception.MoMoException;
+//import com.mservice.shared.sharedmodels.Environment;
+
+//import com.mservice.allinone.processor.allinone.PayATM;
+//import com.mservice.allinone.processor.allinone.CaptureMoMo;
+//import com.mservice.allinone.processor.allinone.RefundMoMo;
+//import com.mservice.allinone.processor.allinone.RefundATM;
+import com.mservice.allinone.models.PayGateRequest;
+
+import com.mservice.shared.utils.Encoder;
+
+import com.mservice.allinone.processor.allinone.CaptureMoMo;
+
+
+//https://github.com/momo-wallet/java/blob/master/src/main/java/com/mservice/pay/models/QRNotifyRequest.java
+import com.mservice.pay.models.QRNotifyRequest;
 
 public class TestMomo {
 
 	// https://business.momo.vn/merchant/integrateInfo
 	// https://test-payment.momo.vn/gw_payment/transactionProcessor
 	
-	String partnerCode = "";
-	String accessKey = "";
-	String secretKey = "";
+//	String partnerCode = "";
+//	String accessKey = "";
+//	String secretKey = "";
 	
-	PartnerInfo devInfo = new PartnerInfo(partnerCode, accessKey, secretKey);
+//	PartnerInfo devInfo = new PartnerInfo(partnerCode, accessKey, secretKey);
+	
+	
+	String requestId = String.valueOf(System.currentTimeMillis());
+    String orderId = String.valueOf(System.currentTimeMillis());
+    
+    String orderInfo = "Pay With MoMo";
+    String returnURL = "https://google.com.vn";
+    String notifyURL = "https://google.com.vn";
+    String extraData = "";
+    String bankCode = "SML";
+    String customerNumber = "0963181714";
+
+    EnvTarget target = EnvTarget.DEV;
+    
+    // PAY_GATE, APP_IN_APP, PAY_POS, PAY_QUERY_STATUS, PAY_REFUND, PAY_CONFIRM;
+	ProcessType process = ProcessType.PAY_GATE;
+    Environment environment = Environment.selectEnv(target, process);
+    
+//    PayGate
+    
+    
+    public void test() throws InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
+    	String secretKey = environment.getPartnerInfo().getSecretKey();
+    	String data = "partnerCode=MOMO&accessKey=F8BBA842ECF85&requestId=MM1540456472575&amount=150000&orderId=MM1540456472575&orderInfo=SDK team.&returnUrl=https://momo.vn&notifyUrl=https://momo.vn&extraData=email=abc@gmail.com";
+    	String signature = Encoder.signHmacSHA256(data, secretKey);
+    	
+    	
+//    	TransactionQuery.process(env, partnerRefId, secretKey, publicKey, momoTransId, version)
+//    	PayGateRequest asdasd = new PayGateRequest(partnerCode, signature, signature, accessKey, amount, signature, data, signature, notifyUrl, returnUrl, requestType)
+    	
+    	QRNotifyRequest qr = new QRNotifyRequest(status, signature, amount, error, partnerRefId, momoTransId, message, partnerCode, accessKey, partnerTransId, transType, responseTime, storeId)
+    	
+    	
+    }
+    
 }
---
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/TestMomo2.java
@@ -0,0 +1,55 @@
+package vn.com.unit.controller;
+
+import com.mservice.allinone.models.*;
+import com.mservice.allinone.processor.allinone.CaptureMoMo;
+import com.mservice.allinone.processor.allinone.PayATM;
+//import com.mservice.allinone.processor.allinone.PaymentResult;
+import com.mservice.allinone.processor.allinone.QueryStatusTransaction;
+import com.mservice.shared.exception.MoMoException;
+import com.mservice.shared.sharedmodels.Environment;
+//import com.mservice.shared.utils.LogUtils;
+
+//https://github.com/momo-wallet/java/blob/master/src/main/java/com/mservice/pay/models/QRNotifyRequest.java
+import com.mservice.pay.models.QRNotifyRequest;
+
+public class TestMomo2 {
+
+//	https://github.com/momo-wallet/java/blob/master/src/main/java/com/mservice/allinone/AllInOne.java
+
+	public void pament() throws MoMoException {
+
+		try {
+			String requestId = String.valueOf(System.currentTimeMillis());
+			String orderId = String.valueOf(System.currentTimeMillis());
+			long amount = 50000;
+
+			String orderInfo = "Pay With MoMo";
+			String returnURL = "https://google.com.vn";
+			String notifyURL = "https://google.com.vn";
+			String extraData = "";
+			String bankCode = "SML";
+
+			Environment environment = Environment.selectEnv("dev", Environment.ProcessType.PAY_GATE);
+
+			// Remember to change the IDs at enviroment.properties file
+
+//	    Payment Method- Phương thức thanh toán
+			CaptureMoMoResponse captureMoMoResponse = CaptureMoMo.process(environment, orderId, requestId,
+					Long.toString(amount), orderInfo, returnURL, notifyURL, "");
+
+//	    Transaction Query - Kiểm tra trạng thái giao dịch
+
+			QueryStatusTransactionResponse queryStatusTransactionResponse = QueryStatusTransaction.process(environment,
+					orderId, requestId);
+
+			// Process Payment Result - Xử lý kết quả thanh toán
+//	    PayGateResponse payGateResponse = PaymentResult.process(environment,new PayGateResponse());
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
