b126807d108b08e8c66cded500992eb30cc19409
hoangnganst <nguyenhoangnganst@gmail.com>
2020-08-12 15:41:39 +0700

    create-func-find-all-account

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/AccountController.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/AccountController.java
@@ -1,18 +1,28 @@
 package vn.com.unit.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import vn.com.unit.entity.Account;
+import vn.com.unit.service.AccountService;
+
 @Controller
 @PreAuthorize("hasRole('ROLE_ADMIN')")
 
 public class AccountController {
+	@Autowired
+	private AccountService accountService;
+	
 	@RequestMapping("/admin/account/list")
 	public ModelAndView accountList(Model model) {
-
+		List<Account> accounts = accountService.findAllAccount();
+		model.addAttribute("accounts",accounts);
 		return new ModelAndView("admin/account/account-table");
 	}
 
