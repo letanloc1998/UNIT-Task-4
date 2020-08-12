975de72cd642f2972f29656c17cce8aaf5d32080
hoangnganst <nguyenhoangnganst@gmail.com>
2020-08-12 15:08:50 +0700

    Create AccountController.java

---
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/AccountController.java
@@ -0,0 +1,19 @@
+package vn.com.unit.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+
+public class AccountController {
+	@RequestMapping("/admin/account/list")
+	public ModelAndView accountList(Model model) {
+
+		return new ModelAndView("admin/account/account-table");
+	}
+
+}
