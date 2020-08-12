dc0c45075783c4142d3035eff4cf86abeb1d35fb
trantrungkhanh <62841543+trantrungkhanh@users.noreply.github.com>
2020-08-12 16:43:21 +0700

    show data cureent

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/VendorController.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/VendorController.java
@@ -1,15 +1,31 @@
 package vn.com.unit.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import vn.com.unit.service.AccountService;
+import vn.com.unit.service.RoleService;
+import vn.com.unit.entity.Account;
+import vn.com.unit.entity.Role;
+import vn.com.unit.entity.AccountRole;
+
+
+
+
 @Controller
 @PreAuthorize("hasRole('ROLE_ADMIN')")
 
 public class VendorController {
+	
+	@Autowired
+	AccountService accountService;
+
+	@Autowired
+	RoleService roleService;
 
 	@RequestMapping("/vendor/home")
 
@@ -18,10 +34,10 @@ public class VendorController {
 		return new ModelAndView("vendor/vendor");
 	}
 	
-	  @RequestMapping("/vendor/myaccount") public ModelAndView accountList(Model
-	  model) {
-	  
-	  return new ModelAndView("vendor/myAccount/account-table"); }
+	@RequestMapping("/vendor/myaccount") public ModelAndView accountList(Model model) {
+		Account account = accountService.getCurrentAccount();	
+		model.addAttribute("current_account", account);
+		return new ModelAndView("vendor/myAccount/account-table"); }
 	 
 
 }
