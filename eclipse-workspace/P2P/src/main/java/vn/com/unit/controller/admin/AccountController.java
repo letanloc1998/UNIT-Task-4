e4b13f663311c42a13d0338baf420a7a32cc6bb9
hoangnganst <nguyenhoangnganst@gmail.com>
2020-08-13 10:49:41 +0700

    create-panigation

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/admin/AccountController.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/admin/AccountController.java
@@ -2,14 +2,19 @@ package vn.com.unit.controller.admin;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import vn.com.unit.entity.Account;
+import vn.com.unit.pageable.PageRequest;
+import vn.com.unit.pageable.pageItem;
 import vn.com.unit.service.AccountService;
 
 @Controller
@@ -20,9 +25,19 @@ public class AccountController {
 	private AccountService accountService;
 	
 	@RequestMapping("/admin/account/list")
-	public ModelAndView accountList(Model model) {
+	public ModelAndView accountList(
+			Model model,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page,
+			@RequestParam(value = "limit", required = false, defaultValue = "10") int limit,
+			HttpServletRequest request) {
+		int totalitems= 50;
+		int totalpages = (int) Math.ceil((double) totalitems/(double) limit);
+		PageRequest pageble = new PageRequest(page, limit,totalitems,totalpages);
+		
 		List<Account> accounts = accountService.findAllAccount();
 		model.addAttribute("accounts",accounts);
+		model.addAttribute("pageble",pageble);
+
 		return new ModelAndView("admin/account/account-table");
 	}
 
