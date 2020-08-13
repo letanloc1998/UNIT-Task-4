93d512ce152554440f42008c9898060506ba9d14
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-13 18:15:45 +0700

    Webhook

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/webhook/MomoWebhook2.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/webhook/MomoWebhook2.java
@@ -15,7 +15,6 @@ import com.mservice.shared.sharedmodels.Environment.EnvTarget;
 import com.mservice.shared.sharedmodels.Environment.ProcessType;
 
 import vn.com.unit.model.MomoPaymentResult;
-import vn.com.unit.service.LogService;
 
 // Handler response from Momo server after payment
 // Thông thường kết quả thanh toán đi theo cách IPN sẽ tới trước callback returnUrl (3s)
@@ -27,9 +26,6 @@ public class MomoWebhook {
 
 	ProcessType process = ProcessType.PAY_GATE;
 	Environment environment = Environment.selectEnv(target, process);
-	
-	@Autowired
-	LogService logService;
 
 	@PostMapping(value = "/webhook/momo", produces = "application/json; charset=UTF-8")
 	public ResponseEntity<String> webhookMomo(@RequestParam Map<String, String> body)
@@ -37,34 +33,17 @@ public class MomoWebhook {
 
 		MomoPaymentResult momoPaymentResult = new MomoPaymentResult(body);
 		
-		String log = "";
-		String rawBody =  momoPaymentResult.toString();
-		String type = "webhook";
-		String author = this.getClass().getName();
-
-		log = "/webhook/momo body : " + rawBody;
-		logService.saveLog(log, type, author);
-		
 		if (momoPaymentResult.checkSignature()) {
-			
 			if (momoPaymentResult.getErrorCode().equals("0")) {
 				// Handler success
 				// Refund if empty product in repository
-				log = "/webhook/momo payment success : " + rawBody;
-				logService.saveLog(log, type, author);
-				
 			} else {
 				// Handler error
-				log = "/webhook/momo payment error : " + rawBody;
-				logService.saveLog(log, type, author);
 			}
 
 			// Return for Momo
 			return ResponseEntity.ok(null);
 		}
-		
-		log = "/webhook/momo wrong signature : " + rawBody;
-		logService.saveLog(log, type, author);
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
 
