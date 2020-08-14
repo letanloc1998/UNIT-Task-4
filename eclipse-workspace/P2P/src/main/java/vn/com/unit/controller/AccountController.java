01d65a6a962e36eee037409ff4ae0d773b2670f1
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-14 17:26:34 +0700

    Register

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/AccountController.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/AccountController.java
@@ -1,13 +1,13 @@
 package vn.com.unit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import vn.com.unit.entity.Account;
 import vn.com.unit.service.AccountService;
@@ -20,8 +20,29 @@ public class AccountController {
 	
 	@PostMapping("/account")
 	@ResponseBody
-	public ModelAndView createAccount(@RequestBody Account account, Model model){
-		accountService.createNewAccount(account, "ROLE_USER");
-		return new ModelAndView("redirect:/");
+	public ResponseEntity<String> createAccount(@RequestBody Account account, Model model){
+		if (account.getUsername() == null || account.getUsername().equals("")) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{ \"msg\" : \"Username cannot be empty\" }");
+		}
+		
+		if (account.getPassword() == null || account.getPassword().equals("")) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{ \"msg\" : \"Password cannot be empty\" }");
+		}
+		
+		if (account.getPassword().length() < 8) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{ \"msg\" : \"Password too short - minimum length is 8 characters\" }");
+		}
+		
+		if (accountService.findByUsername(account.getUsername()) != null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{ \"msg\" : \"Username already exists\" }");
+		}
+		
+		Account account_new = accountService.createNewAccount(account, "ROLE_USER");
+
+		if (account_new != null) {
+			return ResponseEntity.ok("{ \"id\" : " + account_new.getId().toString() + ", \"msg\" : \"Create account successfully\" }");
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{ \"msg\" : \"You can't create an account right now. Try again later\" }");
 	}
 }
