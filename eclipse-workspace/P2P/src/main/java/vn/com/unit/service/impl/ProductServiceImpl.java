cfdd5f1b8bc085a539549e70d539ecbab7dd99fa
trantrungkhanh <62841543+trantrungkhanh@users.noreply.github.com>
2020-08-14 16:32:28 +0700

    fix

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/service/impl/ProductServiceImpl.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/service/impl/ProductServiceImpl.java
@@ -8,10 +8,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import vn.com.unit.entity.Account;
+import vn.com.unit.entity.Brand;
+import vn.com.unit.entity.Category;
 import vn.com.unit.entity.Product;
 import vn.com.unit.entity.Role;
+import vn.com.unit.repository.BrandRepository;
+import vn.com.unit.repository.CategoryRepository;
 import vn.com.unit.repository.ProductRepository;
 import vn.com.unit.repository.ShopRepository;
+import vn.com.unit.service.BrandService;
 import vn.com.unit.service.ProductService;
 import vn.com.unit.service.ShopService;
 
@@ -25,22 +30,26 @@ public class ProductServiceImpl implements ProductService {
 	@Autowired
 	ProductRepository productRepository;
 	
+	@Autowired
+	BrandRepository brandRepository;
 	
+	@Autowired
+	CategoryRepository categoryRepository;
 	
 	@Override
 	public List<Product> findAllProductByShopId(Long shop_id) {
-		List<Product> products = new ArrayList<Product>();
-		try {
-			products = productRepository.findAllProductByShopId(shop_id);
-
-			for (Product product : products) {
-				List<Brand> brands = roleService.findRoleByAccountId(account.getId());
-				account.setRoles(roles);
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		return products;
+		/*
+		 * List<Product> products = new ArrayList<Product>(); try {
+		 * 
+		 * 
+		 * for (Product product : products) { Brand brand =
+		 * brandRepository.findBrandByProductId(product.getId());
+		 * product.setBrand_name(brand);; Category category =
+		 * categoryRepository.findCategoryByProductId(product.getId());
+		 * product.setCategory_name(category); } } catch (Exception e) { // TODO: handle
+		 * exception }
+		 */
+		return productRepository.findAllProductByShopId(shop_id);
 	}
 	
 
