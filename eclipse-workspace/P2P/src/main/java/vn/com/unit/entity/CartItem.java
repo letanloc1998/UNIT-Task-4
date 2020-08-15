178214ae476ebe407e13cb7e06bbc2b9cac2f80d
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-15 23:09:42 +0700

    Update CartItem.java

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/entity/CartItem.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/entity/CartItem.java
@@ -1,5 +1,47 @@
 package vn.com.unit.entity;
 
+import java.util.Date;
+
 public class CartItem {
 
+	private Long account;
+	
+	private Long product;
+	
+	private Long quantity;
+	
+	private Date createAt;
+
+	public Long getAccount() {
+		return account;
+	}
+
+	public void setAccount(Long account) {
+		this.account = account;
+	}
+
+	public Long getProduct() {
+		return product;
+	}
+
+	public void setProduct(Long product) {
+		this.product = product;
+	}
+
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
 }
