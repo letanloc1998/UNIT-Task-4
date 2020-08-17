16e3993b5220e40e97bcbb181ad477f89a9fb474
trantrungkhanh <62841543+trantrungkhanh@users.noreply.github.com>
2020-08-17 22:45:40 +0700

    fix row product

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/ShopProductController.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/ShopProductController.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import vn.com.unit.entity.Brand;
 import vn.com.unit.entity.Category;
 import vn.com.unit.entity.Product;
 import vn.com.unit.entity.Shop;
+import vn.com.unit.service.BrandService;
+import vn.com.unit.service.CategoryService;
 import vn.com.unit.service.ProductService;
 import vn.com.unit.service.ShopService;
 
@@ -23,7 +25,10 @@ public class ShopProductController {
 	ProductService productService;
 	@Autowired
 	ShopService shopService;
-	
+	@Autowired
+	BrandService brandService;
+	@Autowired
+	CategoryService categoryService;
 	
 	@GetMapping("/shop/{shop_id}")
     public ModelAndView home(Model model, @PathVariable ("shop_id") Long shop_id) {
@@ -31,6 +36,11 @@ public class ShopProductController {
 		List<Product> products = productService.findAllProductByShopId(shop_id);
 		model.addAttribute("products", products);
 		model.addAttribute("shop", shop);
+		
+		List<Brand> brands = brandService.findAllBrand();
+		List<Category> categories = categoryService.findAllCategory();
+		model.addAttribute("brands", brands);
+		model.addAttribute("categories", categories);
 
 		model.addAttribute("title", shop.getName());
         return new ModelAndView("shop");
