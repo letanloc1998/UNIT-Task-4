55f8cc27d3becedbc81b4838a8d840796d5968d2
trantrungkhanh <62841543+trantrungkhanh@users.noreply.github.com>
2020-08-19 15:20:53 +0700

    fix 404 and null

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/ProductRepository_findAllProductByCategoryIdAndBrandId.sql
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/ProductRepository_findAllProductByCategoryIdAndBrandId.sql
@@ -1,14 +1,14 @@
 select *
 from p2p_product
-/*BEGIN*/
-  WHERE
-   disable = 'false' AND shop=/*shop_id*/ AND
-  /*IF category_id != null*/
-        category = /*category_id*/
-  /*END*/
-  /*IF brand_id != null*/
-     AND brand = /*brand_id*/
-  /*END*/
+WHERE
+disable = 'false' AND shop=/*shop_id*/
+
+/*IF category_id != null*/
+    AND category = /*category_id*/
+/*END*/
+
+/*IF brand_id != null*/
+    AND brand = /*brand_id*/
 /*END*/
 order by id
 OFFSET  /*offset*/ ROWS FETCH NEXT  /*sizeOfPage*/ ROWS ONLY
