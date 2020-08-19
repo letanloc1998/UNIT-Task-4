0a4f6c29a5c118f362bc526dc75a6b971522c26a
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-19 10:31:53 +0700

    Fix

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/HomeController.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/controller/HomeController.java
@@ -1,5 +1,6 @@
 package vn.com.unit.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,62 +8,93 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import vn.com.unit.entity.Account;
 import vn.com.unit.entity.Product;
+import vn.com.unit.service.AccountService;
 import vn.com.unit.service.CategoryService;
 import vn.com.unit.service.ProductService;
+import vn.com.unit.service.RoleService;
 import vn.com.unit.utils.CommonUtils;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	CategoryService categoryService;
+
 	@Autowired
-	
 	ProductService productService;
 
+	@Autowired
+	private RoleService roleService;
+
+	@Autowired
+	private AccountService accountService;
+
 	@GetMapping("*")
-    public ModelAndView home(Model model, @Param("name") String name) {
-		
+	public ModelAndView home(Model model, @Param("name") String name) {
+
 		model.addAllAttributes(CommonUtils.getMapHeaderAtribute(model, categoryService));
-		
+
+		// Add Role if reload
+
+		try {
+			Account account = accountService.findCurrentAccount();
+
+			if (account != null) {
+				Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+				List<GrantedAuthority> authorities = new ArrayList<>();
+
+				authorities = roleService.findAuthorities(account);
+
+				Authentication newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(),
+						auth.getCredentials(), authorities);
+
+				SecurityContextHolder.getContext().setAuthentication(newAuth);
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
 		model.addAttribute("title", "Home");
-		
+
 //		List<Category> categories = categoryService.findAllCategory();
 //		model.addAttribute("categories", categories);
-		
-		
-		  List<Product> product = productService.findAllProduct();
-		  model.addAttribute("product", product);
-		 
-        return new ModelAndView("index");
-    }
-	
+
+		List<Product> product = productService.findAllProduct();
+		model.addAttribute("product", product);
+
+		return new ModelAndView("index");
+	}
+
 	@GetMapping("/product-by-search")
-    public ModelAndView test(Model model, @RequestParam("name") String name) {
-		 
+	public ModelAndView test(Model model, @RequestParam("name") String name) {
+
 		List<Product> products = productService.findProductByName(name);
 		model.addAttribute("products", products);
 		model.addAttribute("name", name);
-	    
-        return new ModelAndView("product-by-search");
-    }
-	
-	
-	
+
+		return new ModelAndView("product-by-search");
+	}
 
 	@GetMapping("/register")
-    public ModelAndView register(Model model) {
-		
-        return new ModelAndView("register");
-    }
-	
+	public ModelAndView register(Model model) {
+
+		return new ModelAndView("register");
+	}
+
 	@GetMapping("/login")
 	public ModelAndView login(Model model) {
 
@@ -80,7 +112,5 @@ public class HomeController {
 
 		return "redirect:/";
 	}
-	
-	
-	
+
 }
