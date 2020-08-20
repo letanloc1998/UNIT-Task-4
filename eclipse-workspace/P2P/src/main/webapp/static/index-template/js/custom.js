5e6d522875c8b02c3c69ffcd5c3c3989fff1a43f
trantrungkhanh <62841543+trantrungkhanh@users.noreply.github.com>
2020-08-20 11:43:31 +0700

    fix loop js

--- eclipse-workspace/P2P/src/main/webapp/static/index-template/js/custom.js
+++ eclipse-workspace/P2P/src/main/webapp/static/index-template/js/custom.js
@@ -61,6 +61,7 @@ $(document).ready(function()
 	initViewedSlider();
 	initBrandsSlider();
 	initTimer();
+		initQuantity();
 
 	$(window).on('resize', function()
 	{
@@ -70,6 +71,44 @@ $(document).ready(function()
 	});
 
 
+
+
+	/* 
+
+	7. Init Quantity
+
+	*/
+
+	function initQuantity()
+	{
+		// Handle product quantity input
+		if($('.product_quantity').length)
+		{
+			var input = $('#quantity_input');
+			var incButton = $('#quantity_inc_button');
+			var decButton = $('#quantity_dec_button');
+
+			var originalVal;
+			var endVal;
+
+			incButton.on('click', function()
+			{
+				originalVal = input.val();
+				endVal = parseFloat(originalVal) + 1;
+				input.val(endVal);
+			});
+
+			decButton.on('click', function()
+			{
+				originalVal = input.val();
+				if(originalVal > 0)
+				{
+					endVal = parseFloat(originalVal) - 1;
+					input.val(endVal);
+				}
+			});
+		}
+	}
 
 
 
