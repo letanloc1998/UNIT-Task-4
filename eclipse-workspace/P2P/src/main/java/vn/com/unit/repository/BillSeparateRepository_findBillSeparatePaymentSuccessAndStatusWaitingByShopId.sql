877b9ec373426e15721e8c71c6a9bf2c8bfee5e8
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-20 15:15:56 +0700

    Update BillSeparateRepository_findBillSeparatePaymentSuccessAndStatusWaitingByShopId.sql

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillSeparateRepository_findBillSeparatePaymentSuccessAndStatusWaitingByShopId.sql
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillSeparateRepository_findBillSeparatePaymentSuccessAndStatusWaitingByShopId.sql
@@ -1,8 +1,9 @@
 declare @shop int = /*shop_id*/;
 
-select bill_separate.*, p2p_bill.payment
+select bill_separate.*, bill.payment, bill.create_at
 from p2p_bill_separate bill_separate
-left join p2p_bill
-on p2p_bill.id = bill_separate.bill
+left join p2p_bill bill
+on bill.id = bill_separate.bill
 where bill_separate.shop = @shop
-	and bill_separate.status = 0;
+	and bill_separate.status = 0
+	and bill.payment > 0;
