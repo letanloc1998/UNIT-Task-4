ca36e0295bceb33c7f55c43cbd3eac71d409ee13
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-20 12:00:42 +0700

    Bill Separate Success

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillRepository_addBillItemFromCart.sql
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillRepository_addBillItemFromCart.sql
@@ -1,21 +1,5 @@
---insert into p2po_bill_item (id, product, quantity)
---	(
---	select /*bill_id*/ as id, p2po_cart.product, p2po_cart.quantity
---	from p2po_cart
---	where p2po_cart.account = /*account_id*/
---	);
-
---delete from p2po_cart
---where account = /*account_id*/;
-
-
--- @bill_id : param
---insert into p2po_bill (account, address) values (1, '1');
---declare @bill_id int = (select scope_identity());
 declare @bill_id int = /*bill_id*/
 
--- @curent_accoun : param
---declare @curent_account int = 1;
 declare @curent_account int = /*account_id*/
 
 -- create bill separate
@@ -24,7 +8,12 @@ insert into p2po_bill_separate (bill, shop)
 	from 
 		(
 		select distinct product.shop
-		from p2po_cart cart
+		from
+			(
+				select product
+				from p2po_cart
+				where p2po_cart.account = @curent_account
+			) cart
 		left join p2po_product product
 		on product.id = cart.product
 		) temp;
@@ -34,14 +23,9 @@ declare @i int =
 	select count(distinct product.shop)
 	from
 		(
-		select product
-		from
-			(
 			select product
 			from p2po_cart
 			where p2po_cart.account = @curent_account
-			) cart
-		where p2po_cart.account = @curent_account
 		) cart
 	left join p2po_product product
 	on product.id = cart.product
