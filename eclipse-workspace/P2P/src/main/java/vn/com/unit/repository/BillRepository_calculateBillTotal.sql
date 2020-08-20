1d79b7241ca5849a0fee4d38ba51edac101fa352
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-20 13:40:55 +0700

    Update BillRepository_calculateBillTotal.sql

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillRepository_calculateBillTotal.sql
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillRepository_calculateBillTotal.sql
@@ -1,15 +1,15 @@
 select sum(bill.price)
 from
 	(
-		select bill_item.id, (bill_item.quantity * product.price) as price
+		select (bill_item.quantity * product.price) as price
 		from
 			(
 				select bill_item.*
 				from p2po_bill_item bill_item
 				left join p2po_bill_separate bill_separate
 				on bill_separate.id = bill_item.id
-				where bill_separate.bill = /*bill_id*/
+				where bill_separate.bill = 1
 			) bill_item
 		left join p2po_product product
 		on product.id = bill_item.product
-	)
+	) bill
