5b9a4a4ade0a1506e5cf2644a1d5f58bc0122b7b
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-20 15:05:57 +0700

    Update BillSeparateServiceImpl.java

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/service/impl/BillSeparateServiceImpl.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/service/impl/BillSeparateServiceImpl.java
@@ -3,11 +3,15 @@ package vn.com.unit.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import vn.com.unit.entity.BillSeparate;
 import vn.com.unit.repository.BillSeparateRepository;
 import vn.com.unit.service.BillSeparateService;
 
+@Service
+@Transactional
 public class BillSeparateServiceImpl implements BillSeparateService {
 
 	@Autowired
