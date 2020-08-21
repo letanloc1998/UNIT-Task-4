117e4f2000500ed7b7e7c538168ca072352fa5e4
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-21 11:59:28 +0700

    Fix Not Finish

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/service/ChatServerService.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/service/ChatServerService.java
@@ -5,23 +5,38 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.stereotype.Service;
 
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketConfig;
+import com.corundumstudio.socketio.SocketIOServer;
+
 import vn.com.unit.socket.ChatServerSocket;
 
 @Service
 public class ChatServerService {
 
-	private ChatServerSocket server;
+//	private ChatServerSocket server = new ChatServerSocket();
 
 	@PostConstruct
 	public void initChatServerSocket() {
 
-		server = new ChatServerSocket();
+//		server = new ChatServerSocket();
+//		server.start();
+		
+		Configuration config = new Configuration();
+		config.setHostname("localhost");
+		config.setPort(8088);
+		SocketConfig socketConfig = new SocketConfig();
+		socketConfig.setReuseAddress(true);
+
+		config.setSocketConfig(socketConfig);
+
+		SocketIOServer server = new SocketIOServer(config);
 		server.start();
 	}
 
 	@PreDestroy
 	public void stopChatServerSocket() {
-		server.stop();
+//		server.stop();
 	}
 
 }
