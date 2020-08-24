96875f0437e02f11fcc7bb32e248a6fb398688dc
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-24 14:06:03 +0700

    Server Chat Error Session Registry

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/socket/ChatMessage.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/socket/ChatMessage.java
@@ -1,10 +1,5 @@
 package vn.com.unit.socket;
 
-import java.util.Map;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class ChatMessage {
 
 	private String message;
