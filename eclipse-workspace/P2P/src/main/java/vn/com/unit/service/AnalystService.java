f8bbb926a71aeb511e7e1cd542c1b03a85230319
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-25 17:30:08 +0700

    Dashboard

---
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/service/AnalystService.java
@@ -0,0 +1,9 @@
+package vn.com.unit.service;
+
+public interface AnalystService {
+
+	public Long caculateShopRevenueLastDayByShopId(Long shop_id);
+
+	public Long caculateBillErrorLastDayByShopId(Long shop_id);
+
+}
---
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/service/impl/AnalystServiceImpl.java
@@ -0,0 +1,28 @@
+package vn.com.unit.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import vn.com.unit.entity.Revenue;
+import vn.com.unit.repository.RevenueRepository;
+import vn.com.unit.service.AnalystService;
+
+@Service
+@Transactional
+public class AnalystServiceImpl implements AnalystService {
+
+	@Autowired
+	RevenueRepository revenueRepository;
+	
+	@Override
+	public Long caculateShopRevenueLastDayByShopId(Long shop_id) {
+		return revenueRepository.caculateShopRevenueLastDayByShopId(shop_id);
+	}
+
+	@Override
+	public Long caculateBillErrorLastDayByShopId(Long shop_id) {
+		return revenueRepository.caculateBillErrorLastDayByShopId(shop_id);
+	}
+
+}
