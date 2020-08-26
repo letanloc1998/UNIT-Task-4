3b86bc3f274f8e22328fe6a0229059b9598ff9a8
DESKTOP-SG209F6\cuong <cuongtq.tvh@gmail.com>
2020-08-26 18:41:58 +0700

    fix-product

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/ProductRepository_findAllProduct.sql
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/ProductRepository_findAllProduct.sql
@@ -1,3 +1,6 @@
-select * 
-from p2p_product
-where disable = 0
+select distinct a.*
+  from p2p_product a, p2p_category b, p2p_shop c
+  where a.category = b.id and b.disable = 0 and a.disable = 0
+  
+  
+  
