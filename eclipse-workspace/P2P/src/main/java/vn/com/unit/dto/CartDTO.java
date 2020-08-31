661f397859cf986ea6920ac58b58d96768b39a9c
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-31 14:03:44 +0700

    Update CartDTO.java

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/dto/CartDTO.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/dto/CartDTO.java
@@ -1,5 +1,37 @@
 package vn.com.unit.dto;
 
-public class CartDTO {
+import vn.com.unit.entity.Cart;
 
+public class CartDTO extends Cart {
+
+	private String img;
+
+	private String name;
+
+	private Long price;
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getPrice() {
+		return price;
+	}
+
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+	
 }
