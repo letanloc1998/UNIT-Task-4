30f2e4d47798f67b4eea9925c9edb48ae484ea29
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-31 11:00:32 +0700

    Update Log.java

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/entity/Log.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/entity/Log.java
@@ -2,45 +2,19 @@ package vn.com.unit.entity;
 
 import java.util.Date;
 
+import jp.sf.amateras.mirage.annotation.Column;
+
 public class Log {
 
+	@Column(name = "log")
 	private String log;
 	
+	@Column(name = "type")
 	private String type;
 	
+	@Column(name = "author")
 	private String author;
-	
+
+	@Column(name = "create_at")
 	private Date createAt;
-	public String getLog() {
-		return log;
-	}
-
-	public void setLog(String log) {
-		this.log = log;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public Date getCreateAt() {
-		return createAt;
-	}
-
-	public void setCreateAt(Date createAt) {
-		this.createAt = createAt;
-	}
-
 }
