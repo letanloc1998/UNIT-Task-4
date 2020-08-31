6487612a9750decc1aecfc7791eb953a10619cad
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-31 11:42:09 +0700

    Update AccountRoleDto.java

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/dto/AccountRoleDto.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/dto/AccountRoleDto.java
@@ -1,5 +1,20 @@
 package vn.com.unit.dto;
 
-public class AccountRoleDto {
+import java.util.List;
+
+import vn.com.unit.entity.Account;
+import vn.com.unit.entity.Role;
+
+public class AccountRoleDto extends Account {
+
+	List<Role> role;
+
+	public List<Role> getRole() {
+		return role;
+	}
+
+	public void setRole(List<Role> role) {
+		this.role = role;
+	}
 
 }
