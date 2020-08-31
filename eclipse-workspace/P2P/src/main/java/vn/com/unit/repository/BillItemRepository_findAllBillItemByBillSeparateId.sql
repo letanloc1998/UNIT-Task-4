3bb3093dc847d53a29b74f71b27deb49f679a4ef
trantrungkhanh <62841543+trantrungkhanh@users.noreply.github.com>
2020-08-31 17:29:09 +0700

    fix bill item

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillItemRepository_findAllBillItemByBillSeparateId.sql
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillItemRepository_findAllBillItemByBillSeparateId.sql
@@ -1,1 +1,9 @@
-SELECT * FROM p2p_bill_item WHERE id = /*bill_separate_id*/
+SELECT p2p_bill_item.*, p2p_product.name as product_name, p2p_brand.name as brand_name, p2p_category.name as category_name, p2p_product.img, p2p_product.price
+FROM p2p_bill_item 
+left join p2p_product 
+on p2p_bill_item.product = p2p_product.id
+left join p2p_category
+on p2p_product.category = p2p_category.id
+left join p2p_brand
+on p2p_product.brand = p2p_brand.id
+WHERE p2p_bill_item.id = /*bill_separate_id*/
