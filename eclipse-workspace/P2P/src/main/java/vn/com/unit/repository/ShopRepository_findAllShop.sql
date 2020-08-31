9d35a0cf289899a0ffdd60c901cf301add412494
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-31 17:27:39 +0700

    Update

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/ShopRepository_findAllShop.sql
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/ShopRepository_findAllShop.sql
@@ -1,6 +1,6 @@
-SELECT shop.*, account.name owner_name
+SELECT shop.*, account.name username
 FROM  p2p_shop shop
 left Join p2p_account account
-on account.id=shop.id
+on account.id = shop.id
 order by shop.id
 OFFSET /*offset*/ ROWS FETCH NEXT /*sizeOfPage*/ ROWS ONLY
