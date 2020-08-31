40b3c7231b17b071823aae90d6cc3f1c075bc5c8
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-31 16:29:06 +0700

    Update BillSeparateRepository_findBillSeparateByPaymentAndStatusAndShopId.sql

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillSeparateRepository_findBillSeparateByPaymentAndStatusAndShopId.sql
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillSeparateRepository_findBillSeparateByPaymentAndStatusAndShopId.sql
@@ -1,11 +1,11 @@
 declare @shop int = /*shop_id*/;
 
-select p2p_bill_separate.*, p2p_bill.address, p2p_bill.create_at, p2p_account.username
-from p2p_bill_separate 
-left join p2p_bill 
-on p2p_bill.id = p2p_bill_separate.bill
-left join p2p_account
-on p2p_bill.account = p2p_account.id
+select bill_separate.*, bill.address, bill.create_at, account.username
+from p2p_bill_separate bill_separate
+left join p2p_bill bill
+on bill.id = bill_separate.bill
+left join p2p_account account
+on bill.account = account.id
 where bill_separate.shop = @shop
 /*IF payment > 0 */
   and bill.payment > 0
