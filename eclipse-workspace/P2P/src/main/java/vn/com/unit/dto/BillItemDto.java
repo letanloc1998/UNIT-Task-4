61be23ce392fa200ec048c8cb823405106b65f29
letanloc1998 <26348718+letanloc1998@users.noreply.github.com>
2020-08-31 15:34:33 +0700

    Update BillItemDto.java

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/dto/BillItemDto.java
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/dto/BillItemDto.java
@@ -4,23 +4,45 @@ import vn.com.unit.entity.BillItem;
 
 public class BillItemDto extends BillItem {
 
-	private String name;
+	private String productName;
+
+	private String brandName;
 	
+	private String categoryName;
+
 	private String img;
-	
+
 	private Long price;
-	
+
 	public BillItemDto(BillItem billItem) {
 		super(billItem);
 	}
 
-	public String getName() {
-		return name;
+	public String getProductName() {
+		return productName;
+	}
+	
+	public String getBrandName() {
+		return brandName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
 	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	
 
 	public String getImg() {
 		return img;
@@ -37,8 +59,8 @@ public class BillItemDto extends BillItem {
 	public void setPrice(Long price) {
 		this.price = price;
 	}
-	
+
 	public Long getTotal() {
-		return this.getPrice()*this.getQuantity();
+		return this.getPrice() * this.getQuantity();
 	}
 }
