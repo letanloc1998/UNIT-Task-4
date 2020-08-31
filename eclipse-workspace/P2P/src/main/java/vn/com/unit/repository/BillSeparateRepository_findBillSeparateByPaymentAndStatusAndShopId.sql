6cc15b67e896a39627cf8113789b53871900787b
trantrungkhanh <62841543+trantrungkhanh@users.noreply.github.com>
2020-08-31 16:35:38 +0700

    fix sql

--- eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillSeparateRepository_findBillSeparateByPaymentAndStatusAndShopId.sql
+++ eclipse-workspace/P2P/src/main/java/vn/com/unit/repository/BillSeparateRepository_findBillSeparateByPaymentAndStatusAndShopId.sql
@@ -8,16 +8,16 @@ left join p2p_account account
 on bill.account = account.id
 where bill_separate.shop = @shop
 /*IF payment > 0 */
-  and p2p_bill.payment > 0
-  and p2p_bill_separate.status = /*status*/
+  and bill.payment > 0
+  and bill_separate.status = /*status*/
 /*END*/
 /*IF payment < 0 */
-  and p2p_bill.payment < 0
-  and p2p_bill_separate.status = 0
+  and bill.payment < 0
+  and bill_separate.status = 0
   /*END*/
 /*IF payment == 0 */
-  and p2p_bill.payment = 0
-    and p2p_bill_separate.status = 0
+  and bill.payment = 0
+    and bill_separate.status = 0
 /*END*/  
-order by p2p_bill_separate.id desc
+order by bill_separate.id desc
 OFFSET  /*offset*/ ROWS FETCH NEXT  /*sizeOfPage*/ ROWS ONLY
